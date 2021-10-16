f9968b85915a4a32a572f490563f2c69de3d723e
mlreid <mlreid100@gmail.com>
2021-10-16 11:26:32 +0100

    First few lines

--- wg17p2.r
+++ wg17p2.r
@@ -8,3 +8,16 @@ population <- rep(0, n)
 beta <- rlnorm(n, 0, 0.5)
 beta_prob <- beta / mean(beta)
 lambda <- 0.4 / n
+
+gamma <- 1.4 / n # = daily prob of going from S to E
+delta <- 1 / 3 # = daily prob of going from E to I
+theta <- 1 / 5 # = daily prob of going from I to R
+
+#initialize suceptibles
+x <- rep(0, n)
+
+#create 10 exposed
+x[1:10] <- 1
+
+#STorage for pop in each stage for 100 days
+S <- E <- I <- R <- rep(0, 100)
