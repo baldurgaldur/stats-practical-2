6faf51e5e3082d6f976c88a979e6e3f5faa56b05
Baldur <balbjo22@gmail.com>
2021-10-19 10:18:31 +0100

    Replace 3 binomial calls with one runif
    - Shaving our models execution time from 2.1sec to 0.6 for each day
    - Remove commented out code so that we can take stock of where we are

--- wg17p2.r
+++ wg17p2.r
@@ -1,6 +1,22 @@
 # 17 Ava Napper, Baldur Björnsson, Madeleine Reid
 # https://github.com/baldurgaldur/stats-practical-2
 
+report <- function(state_vector, day) {
+    removed_at_start <- sum(state_vector == 3)
+    infected_at_start <- sum(state_vector == 2)
+    susceptible <- sum(state_vector == 0)
+    exposed <- sum(state_vector == 1)
+    print(c("No of susceptible ppl ", susceptible, "at beginning of day: ", i))
+    print(c("No of exposed ppl ", exposed))
+    print(c("No of infected ppl ", infected_at_start))
+    print(c("No of removed ppl ", removed_at_start))
+
+    #print("who is exposed?")
+    #print(which(pop_states["state"] == 1))
+    #print("who is infected?")
+    #print(which(pop_states["state"] == 2))
+}
+
 update_states <- function(pop_states, lambda, i) {
     # One day in our model goes by.
     # Each row in population_states represent an individual in our model
@@ -9,45 +25,24 @@ update_states <- function(pop_states, lambda, i) {
     p_leaving_infected <- 1 / 5
     p_leaving_exposed <- 1 / 3
 
-    removed_at_start <- sum(pop_states["state"] == 3)
-    infected_at_start <- sum(pop_states["state"] == 2)
-    susceptible <- sum(pop_states["state"] == 0)
-    exposed <- sum(pop_states["state"] == 1)
-    print(c("No of susceptible ppl ", susceptible, "at time: ", i))
-    print(c("No of exposed ppl ", exposed))
-    print("who is exposed?")
-    print(which(pop_states["state"] == 1))
-    print("who is infected?")
-    print(which(pop_states["state"] == 2))
-    print(c("No of infected ppl ", infected_at_start))
-    print(c("No of removed ppl ", removed_at_start))
+    report(pop_states$state, i)
 
     #calculate the probability of person j moving into the exposed state
     infected_beta_sum <- sum(pop_states$beta[pop_states["state"] == 2]) * lambda
-    #print(c("infected beta sum", infected_beta_sum))
     infection_prob <- pop_states$beta * infected_beta_sum
 
-    #3 vectors of binomal realisations (S->E, E-> I and I->R) to determine whether an individual moves into the next state
-    #the vector of interest for each individual is determined by which state they are already in
-    # A person cannot move more than one state forward when the model is like this.
-    now_removed <- rbinom(pop_size, 1, p_leaving_infected) * (pop_states == 2)
-    now_infected <- rbinom(pop_size, 1, p_leaving_exposed) * (pop_states == 1)
-    now_exposed <- rbinom(pop_size, 1, infection_prob) * (pop_states == 0)
-    
-    #sum the 3 vectors to determine which individuals will move up a state
-    moving_states <- now_exposed[, 1] + now_infected[, 1] + now_removed[, 1]
-    print("The ones who will move this turn")
-    print(which(moving_states != 0))
+    # We could random fewer and put them in the right place
+    # Eg. no point in randoming for all the ppl in the removed state
+    rand <- runif(pop_size)
 
-    #the new states of each individual
+    now_removed <- pop_states$state == 2 & rand < p_leaving_infected
+    now_infected <- pop_states$state == 1 & rand < p_leaving_exposed
+    now_exposed <- pop_states$state == 0 & rand < infection_prob
+
+    # Combine the 3 vectors to determine which individuals will move up a state
+    moving_states <- now_exposed + now_infected + now_removed
+
     new_states <- moving_states + pop_states$state
-    print("Who is exposed after run")
-    print(which(new_states == 1))
-    print("Who is infected after run")
-    print(which(new_states == 2))
-    print("Who is removed after run")
-    print(which(new_states == 3))
-
     data.frame(state = new_states, beta = pop_states$beta)
 }
 
@@ -55,7 +50,7 @@ update_states <- function(pop_states, lambda, i) {
 ## Constants
 # Population of Scotland
 pop_size <- 5500000
-simulation_days <- 100
+simulation_days <- 3
 lambda <- 0.4 / pop_size
 beta <- rlnorm(pop_size, 0, 0.5)
 beta_prob <- beta / mean(beta)
@@ -72,22 +67,14 @@ pop_states[starting_exposed, "state"] <- 1
 
 daily_state_changes <- matrix(data = 0, nrow = simulation_days, ncol = 2)
 
-# whole_simulation <- function(initial_pop_states, intial_daily_state_changes, lambda, i, simulation_days){
-#     new_pop_states <- initial_pop_states
-#     new_daily_state_changes <- initial_daily_state_changes
-#     for (i in 1:simulation_days) {
-#     new_pop_states, new_daily_state_changes <- pass_one_day(new_pop_states, new_daily_state_changes, lambda, i)
-#     }
-# }
-
-
 for (i in 1:simulation_days) {
-    new_states <- update_states(pop_states, lambda, i)
+    print(system.time(new_states <- update_states(pop_states, lambda, i)))
+    print("one day time above")
     #daily_state_changes <- calc_state_changes(pop_states, new_states, i)
     pop_states <- new_states
 }
 
-print(c("done: ", daily_state_changes))
+print("Pandemic over")
 
 #TODO: Do this
 calc_state_changes <- function(pop_states, new_pop, daily_state_changes, i) {
