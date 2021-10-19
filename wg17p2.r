1c00598ed1d78c46c4e2bd0c426ff236fef131d5
Baldur <balbjo22@gmail.com>
2021-10-19 16:47:22 +0100

    Move constants to the top and time stuff
    Move prob constants to the top instead of calculating each time
    Try out only getting rand for those who need it instead of all.
    Turns out it is actually slower.

--- wg17p2.r
+++ wg17p2.r
@@ -17,15 +17,23 @@ report <- function(state_vector, day) {
     #print(which(pop_states["state"] == 2))
 }
 
+## Constants
+# Population of Scotland
+pop_size <- 5500000
+simulation_days <- 160
+lambda <- 0.4 / pop_size
+beta <- rlnorm(pop_size, 0, 0.5)
+beta_prob <- beta / mean(beta)
+p_leaving_infected <- 1 / 5
+p_leaving_exposed <- 1 / 3
+
 update_states <- function(pop_states, lambda, i) {
     # One day in our model goes by.
     # Each row in population_states represent an individual in our model
     # Each row in daily_state_changes is the difference how many individuals
     # were in that state between the start of the day and the end of the day.
-    p_leaving_infected <- 1 / 5
-    p_leaving_exposed <- 1 / 3
 
-    report(pop_states$state, i)
+    #report(pop_states$state, i))
 
     #calculate the probability of person j moving into the exposed state
     infected_beta_sum <- sum(pop_states$beta[pop_states["state"] == 2]) * lambda
@@ -34,6 +42,11 @@ update_states <- function(pop_states, lambda, i) {
     # We could random fewer and put them in the right place
     # Eg. no point in randoming for all the ppl in the removed state
     rand <- runif(pop_size)
+
+    #need_rand <- pop_states$state != 3
+    #rand_vals <- runif(length(need_rand))
+    #concise_rand <- rep(1, nrow(pop_states))
+    #concise_rand[need_rand] <- rand_vals
 
     now_removed <- pop_states$state == 2 & rand < p_leaving_infected
     now_infected <- pop_states$state == 1 & rand < p_leaving_exposed
@@ -71,15 +84,6 @@ calc_state_changes <- function(pop_states, new_pop, daily_state_changes, i) {
     
 }
 
-
-## Constants
-# Population of Scotland
-pop_size <- 5500000
-simulation_days <- 3
-lambda <- 0.4 / pop_size
-beta <- rlnorm(pop_size, 0, 0.5)
-beta_prob <- beta / mean(beta)
-
 # An appropriate data structure to monitor each person is a data frame.
 # It has the "state" column and the "beta" column corresponding to a single
 # individuals _current_ state and beta value. The states in our model are:
@@ -92,11 +96,12 @@ pop_states[starting_exposed, "state"] <- 1
 
 daily_state_changes <- matrix(data = 0, nrow = simulation_days, ncol = 2)
 
+system.time(
 for (i in 1:simulation_days) {
-    print(system.time(new_states <- update_states(pop_states, lambda, i)))
-    print("one day time above")
+    new_states <- update_states(pop_states, lambda, i)
     #daily_state_changes <- calc_state_changes(pop_states, new_states, i)
-    pop_states <- new_states 
-}
+    pop_states <- new_states
+})
 
 print("Pandemic over")
+report(pop_states$state, 100)
