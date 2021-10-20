9a9d8b1502f872c2dd3ac110149ad9307503849c
AvaNapper <avanap1@gmail.com>
2021-10-20 16:43:19 +0100

    random population change

--- wg17p2.r
+++ wg17p2.r
@@ -20,6 +20,8 @@ report <- function(state_vector, day) {
 ## Constants
 # Population of Scotland
 pop_size <- 5500000
+cautious_pop_size <- pop_size/10
+rand_pop_size <- pop_size/1000
 simulation_days <- 160
 lambda <- 0.4 / pop_size
 beta <- rlnorm(pop_size, 0, 0.5)
@@ -27,6 +29,7 @@ beta_prob <- beta / mean(beta)
 p_leaving_infected <- 1 / 5
 p_leaving_exposed <- 1 / 3
 
+#finds the lowest 10% of betas and the indicies of the corresponding people
 ordered_beta <- sort(beta_prob)
 cautious_beta <- ordered_beta[c(1: pop_size/10)]
 cautious_index <- match(cautious_beta, beta_prob)
@@ -65,20 +68,34 @@ update_states <- function(pop_states, lambda, i) {
 }
 
 #TODO: Do this
-calc_state_changes <- function(pop_states, new_pop, daily_state_changes, i) {
+calc_state_changes <- function(pop_states, new_pop, daily_state_changes, i, pop_size) {
     
+    susceptible_yesterday <- sum(pop_states$state == 0)
+    print(susceptible_yesterday)
     removed_yesterday <- sum(pop_states$state == 3)
-    infected_yesterday <- sum(pop_states$state == 2)
-    
-    removed_new <- sum(new_pop[,1] == 3)
-    infected_new <- sum(new_pop[,1] == 2)
-    
-    difference_in_removed <- removed_new - removed_yesterday
 
-    new_infections <- (infected_new - infected_yesterday) + difference_in_removed
+    susceptible_today <- sum(new_pop$state == 0)
+    removed_today <- sum(new_pop$state == 3)
 
-    daily_state_changes[i, 1] <- new_infections
-    daily_state_changes[i, 2] <- difference_in_removed
+    new_infections <- susceptible_yesterday - susceptible_today
+    total_infections <- pop_size - susceptible_today - removed_today
+
+    # removed_yesterday <- sum(pop_states$state == 3)
+    # infected_yesterday <- sum(pop_states$state == 2)
+    
+    # removed_new <- sum(new_pop[,1] == 3)
+    # infected_new <- sum(new_pop[,1] == 2)
+    
+    # difference_in_removed <- removed_new - removed_yesterday
+
+    # new_infections <- (infected_new - infected_yesterday) + difference_in_removed
+
+    
+
+    daily_state_changes[i, 1] <- susceptible_today
+    daily_state_changes[i, 2] <- removed_today
+    daily_state_changes[i, 3] <- new_infections
+    daily_state_changes[i, 4] <- total_infections
 
     daily_state_changes
 }
@@ -87,43 +104,47 @@ calc_state_changes <- function(pop_states, new_pop, daily_state_changes, i) {
 # It has the "state" column and the "beta" column corresponding to a single
 # individuals _current_ state and beta value. The states in our model are:
 # 0:= Susceptible, 1:= Exposed, 2:= Infected, 3:= Removed(Recovered or dead)
-pop_states <- data.frame(state = rep(0, pop_size), beta = beta_prob, rand = sample(1:pop_size, 1))
+random <- sample(c(1:pop_size), pop_size, replace= FALSE)
+
+pop_states <- data.frame(state = rep(0, pop_size), beta = beta_prob, rand = random )
+
 
 # Prime the model by choosing 10 random ppl to be exposed
 starting_exposed <- sample(1:pop_size, 10)
 pop_states[starting_exposed, "state"] <- 1
 
-
 max_beta <- ordered_beta[pop_size/10]
-max_rand <- pop_size/100
+max_rand <- pop_size/1000
 
 #creates data frames for state_changes for the 3 population samples
-daily_state_changes <- matrix(data = 0, nrow = simulation_days, ncol = 2)
-cautious_daily_changes <- matrix(data = 0, nrow = simulation_days, ncol = 2)
-random_samp_changes <- matrix(data = 0, nrow = simulation_days, ncol = 2)
+daily_state_changes <- matrix(data = 0, nrow = simulation_days, ncol = 4)
+cautious_daily_changes <- matrix(data = 0, nrow = simulation_days, ncol = 4)
+random_samp_changes <- matrix(data = 0, nrow = simulation_days, ncol = 4)
 
 print(system.time(for (i in 1:simulation_days) {
+    print(i)
     print(system.time(new_states <- update_states(pop_states, lambda, i)))
-    print("one day time above")
-    if (i > 1) {
-        daily_state_changes <- calc_state_changes(pop_states, new_states, daily_state_changes, i)
 
-        #finds the cautious people in pop_states and new_states
-        cautious_pop <- pop_states[pop_states$beta <= max_beta,]
-        cautious_new <- new_states[pop_states$beta <= max_beta,]
+    print(system.time(daily_state_changes <- calc_state_changes(pop_states, new_states, daily_state_changes, i, pop_size)))
 
-        cautious_daily_changes <- calc_state_changes(cautious_pop, cautious_new, cautious_daily_changes, i)
+    #finds the cautious people in pop_states and new_states
+    cautious_pop <- pop_states[pop_states$beta <= max_beta,]
+    cautious_new <- new_states[pop_states$beta <= max_beta,]
 
-        #finds people in the random 0.1% sample
-        rand_pop <- pop_states[pop_states$rand <= max_rand,]
-        rand_new <- new_states[new_states$rand <= max_rand,]
+    cautious_daily_changes <- calc_state_changes(cautious_pop, cautious_new, cautious_daily_changes, i, cautious_pop_size)
 
-        random_daily_changes <- calc_state_changes(rand_pop, rand_new, random_samp_changes, i)
-    }
+    #finds people in the random 0.1% sample
+   
+    rand_pop <- pop_states[pop_states$rand <= max_rand,]
+    rand_new <- new_states[new_states$rand <= max_rand,]
+
+    random_daily_changes <- calc_state_changes(rand_pop, rand_new, random_samp_changes, i, rand_pop_size)
+    
     pop_states <- new_states
     
 }))
 
+print('above is time for whole program')
 print(daily_state_changes)
 print(cautious_daily_changes)
 print(random_daily_changes)
