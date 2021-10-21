7f42126c79fcbbac072c68fc2ffce2021b693c7f
AvaNapper <avanap1@gmail.com>
2021-10-21 16:16:57 +0100

    Added comments

--- wg17p2.r
+++ wg17p2.r
@@ -1,15 +1,18 @@
 # 17 Ava Napper, Baldur Björnsson, Madeleine Reid
 # https://github.com/baldurgaldur/stats-practical-2
 
-update_states <- function(pop_states, lambda, i) {
-    # One day in our model goes by.
-    # Each row in population_states represent an individual in our model
-    # Each row in daily_state_changes is the difference how many individuals
-    # were in that state between the start of the day and the end of the day.
+update_states <- function(pop_states, lambda, p_leaving_infected, p_leaving_exposed, i) {
+    #Purpose: to update the state of each individual after the model has run for 1 
+    #Input: data frame of which state each individual is in, before the model has ran
+    #       overall viral infectivity parameter
+    #       probability of leaving the infected state on a given day
+    #       probability of leaving the exposed state on a given day
+    #       day of the simulation
+    #Output: updated data frame of the state each individual is in
+
 
     #calculate the probability of person j moving into the exposed state
     infected_beta_sum <- sum(pop_states$beta[pop_states$state == 2]) * lambda
-
     infection_prob <- pop_states$beta * infected_beta_sum
 
     # We could random fewer and put them in the right place
@@ -28,17 +31,35 @@ update_states <- function(pop_states, lambda, i) {
 }
 
 calc_state_changes <- function(pop_states, new_pop, daily_state_changes, i, pop_size) {
+    #Purpose: to update the data frame of state changes on each day, so that it contains
+    #         today's state changes
+    #       
+    #Input: population specific data frame of which state each individual was in yesterday
+    #       "                   "                   "            "        is in today
+    #       current data frame of state changes on each day, to be updated by the function
+    #       the day of the simulation
+    #       the size of the population
+    #Output: the updated data frame of state changes on each day
+
     
+    #finds how many people were susceptible and removed on previous day
     susceptible_yesterday <- sum(pop_states$state == 0)
     removed_yesterday <- sum(pop_states$state == 3)
 
+    #finds how many people were susceptible and removed on current day
     susceptible_today <- sum(new_pop$state == 0)
     removed_today <- sum(new_pop$state == 3)
 
+    #the number of new infections today is the number of people
+    #who have left the susceptible state in the past day
     new_infections <- susceptible_yesterday - susceptible_today
+
+    #the total infections for today is the total population
+    # minus those in state S or state R
     total_infections <- pop_size - susceptible_today - removed_today
     
-
+    #fills in todays row of the data frame consisting of the 
+    #state changes for that day
     daily_state_changes[i, 1] <- susceptible_today
     daily_state_changes[i, 2] <- removed_today
     daily_state_changes[i, 3] <- new_infections
@@ -49,7 +70,7 @@ calc_state_changes <- function(pop_states, new_pop, daily_state_changes, i, pop_
 
 ## Constants
 # Population of Scotland
-pop_size <- 5500000
+pop_size <- 100000
 cautious_pop_size <- pop_size/10
 rand_pop_size <- pop_size/1000
 
@@ -71,14 +92,15 @@ max_beta <- ordered_beta[pop_size/10]
 #to find a 0.1% random sample we choose people with index lower than this value 
 max_rand <- rand_pop_size
 
-ten_simulations <- matrix(data = 0, nrow = 10, ncol = simulation_days/10)
+ten_simulations <- matrix(data = 0, nrow = 10, ncol = simulation_days)
  
-for (j in 1:1){
+for (j in 1:10){
 
     print(j)
     # 0:= Susceptible, 1:= Exposed, 2:= Infected, 3:= Removed(Recovered or dead)
     random <- sample(c(1:pop_size), pop_size, replace= FALSE)
 
+    # Each row in population_states represent an individual in our model
     pop_states <- data.frame(state = rep(0, pop_size), beta = beta_prob, rand = random )
 
 
@@ -87,13 +109,15 @@ for (j in 1:1){
     pop_states[starting_exposed, "state"] <- 1
 
     #creates data frames for state_changes for the 3 population samples
+    # Each row in daily_state_changes is the difference how many individuals
+    # were in that state between the start of the day and the end of the day.
     daily_state_changes <- matrix(data = 0, nrow = simulation_days, ncol = 4)
     cautious_daily_changes <- matrix(data = 0, nrow = simulation_days, ncol = 4)
     random_samp_changes <- matrix(data = 0, nrow = simulation_days, ncol = 4)
 
     for (i in 1:simulation_days) {
         
-        new_states <- update_states(pop_states, lambda, i)
+        new_states <- update_states(pop_states, lambda, p_leaving_infected, p_leaving_exposed, i)
 
         daily_state_changes <- calc_state_changes(pop_states, new_states, daily_state_changes, i, pop_size)
 
@@ -111,9 +135,10 @@ for (j in 1:1){
         
         pop_states <- new_states
         
-        if (i %% 10 == 0){
-            ten_simulations[j, i/10] <- daily_state_changes[i, 4]
-        }
+        # if (i %% 10 == 0){
+        #     ten_simulations[j, i/10] <- daily_state_changes[i, 4]
+        # }
+        ten_simulations[j, i] <- daily_state_changes[i, 4]
         
     }
     
@@ -149,3 +174,22 @@ points(day_max_random, max_rand_scaled, pch = 19, col = 3,)
 text(day_max_random, max_rand_scaled, labels = paste("(", day_max_random, ",", max_rand_scaled, ")"), pos = 4, cex = 0.5)
 
 print(ten_simulations)
+
+#continous quantiles plot
+ten_median <- rep(0, simulation_days)
+ten_lq <- rep(0, simulation_days)
+ten_uq <- rep(0, simulation_days)
+for (k in 1:simulation_days){
+    ten_median[k] <- median(ten_simulations[,k])
+    ten_lq[k] <- quantile(ten_simulations[,k])[2]
+    ten_uq[k] <- quantile(ten_simulations[,k])[4]
+}
+plot(1:simulation_days, ten_median/ten_median, type="l", xlab = "days", ylab = "Daily Infections per 100 000 people", ylim = c(0, 5), col = 1, cex = 10)
+lines(ten_lq/ten_median, col = 2)
+lines(ten_uq/ten_median, col = 3)
+
+#legend("topleft", legend = c("total population", "cautious population", "random 0.1%"), col = 1:3, lty = 1, cex = 0.5)
+#points(day_max_daily, max_daily_scaled, pch = 19, col = 1)
+#text(day_max_daily, max_daily_scaled, labels = paste("(", day_max_daily, ",", max_daily_scaled, ")"), pos = 4, cex = 0.5)
+
+
