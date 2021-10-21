a127fe6973bece45621b5bdad20bc2b971ecfd00
Baldur <balbjo22@gmail.com>
2021-10-21 21:39:39 +0100

    Review code comments

--- wg17p2.r
+++ wg17p2.r
@@ -12,63 +12,61 @@ update_states <- function(pop_states, lambda, p_leaving_infected, p_leaving_expo
     #Output:    Updated data frame of the state each individual is in when one
     #           day has passed
 
-
-    #calculate the probability of person j moving into the exposed state
+    # Calculate the probability of person j moving into the exposed state
     infected_beta_sum <- sum(pop_states$beta[pop_states$state == 2]) * lambda
     infection_prob <- pop_states$beta * infected_beta_sum
 
     rand <- runif(pop_size)
 
-    #3 vectors, where 1 corresponds to the indiviual changing state
+    # 3 vectors, where 1 corresponds to the indiviual changing state
     # and 0 corresponds to no change of state
     now_removed <- pop_states$state == 2 & rand < p_leaving_infected
     now_infected <- pop_states$state == 1 & rand < p_leaving_exposed
     now_exposed <- pop_states$state == 0 & rand < infection_prob
 
     # Combine the 3 vectors to determine which individuals will move up a state
+    # Note that these are boolean vectors, so a person never moves into
+    # more than one state each day
     moving_states <- now_exposed + now_infected + now_removed
 
-    #add the vector representing those who are moving states to the current states
-    #to get the new state of each individual
+    # Add the vector representing those who are moving states to the current 
+    # states to get the new state of each individual
     new_states <- moving_states + pop_states$state
     data.frame(state = new_states, beta = pop_states$beta, rand = pop_states$rand)
 }
 
 calc_state_changes <- function(pop_states, new_pop, daily_state_changes, i, pop_size) {
-    #Purpose: to update the data frame of state changes on each day, so that it contains
-    #         today's state changes
-    #       
-    #Input: population specific data frame of which state each individual was in yesterday
+    #Purpose:   To update the data frame of state changes on each day,
+    #           so that it contains today's state changes
+    #Input: Population specific data frame of which state each individual was in yesterday
     #       "                   "                   "            "        is in today
     #       current data frame of state changes on each day, to be updated by the function
-    #       the day of the simulation
-    #       the size of the population
-    #Output: the updated data frame of state changes on each day
+    #       The day of the simulation
+    #       The size of the population
+    #Output: The updated data frame of state changes on each day
 
-    
-    #finds how many people were susceptible and removed on previous day
+    # Count how many people were susceptible and removed yesterday
     susceptible_yesterday <- sum(pop_states$state == 0)
-    removed_yesterday <- sum(pop_states$state == 3)
 
-    #finds how many people were susceptible and removed on current day
+    # Count how many people were susceptible and removed today
     susceptible_today <- sum(new_pop$state == 0)
     removed_today <- sum(new_pop$state == 3)
 
-    #the number of new infections today is the number of people
-    #who have left the susceptible state in the past day
+    # The number of new infections today is the number of people
+    # who have left the susceptible state in the past day
     new_infections <- susceptible_yesterday - susceptible_today
 
-    #the total infections for today is the total population
-    # minus those in state S or state R
+    # The total infections for today is the total population
+    # minus those in state S or R
     total_infections <- pop_size - susceptible_today - removed_today
-    
-    #fills in todays row of the data frame consisting of the 
-    #state changes for that day
+
+    # Fill in todays row of the data frame consisting of the
+    # state changes for that day
     daily_state_changes[i, 1] <- susceptible_today
     daily_state_changes[i, 2] <- removed_today
     daily_state_changes[i, 3] <- new_infections
     daily_state_changes[i, 4] <- total_infections
-    
+
     daily_state_changes
 }
 
@@ -77,8 +75,8 @@ plot_simulation_peaks <- function(value_of_peak, day_of_peak) {
     #           We plot the day of the peak for each group in one plot
     #           And in another we plot the percentage of people infected
     #           at the peak of the model run.
-    # Input:    Two matrices. The columns of each are 1 = Total pop, 
-    #           2 = Cautious 10% and 3 = random 0.1%. Each row is the value 
+    # Input:    Two matrices. The columns of each are 1 = Total pop,
+    #           2 = Cautious 10% and 3 = random 0.1%. Each row is the value
     #           corresponding to one run of our model
     # Output:   Display 2 box plots
     boxplot(main="Population percentage infected at pandemic peak", xlab = "Groups of people", names=c("Total", "Cautious 10%", "Random 0.1%"), ylab="Percent of population", value_of_peak)
@@ -97,13 +95,13 @@ peak_day_and_total_infections <- function(daily_state_changes) {
     greatest_infected_number <- max(daily_state_changes[, 4])
     all_peak_days <- which(daily_state_changes[, 4] == greatest_infected_number)
     # When 2 days are the peak, we report the average day. E.g. if day 18 and 19
-    # Have the greatest number of infected, we report day 18.5 to be the max
+    # have the greatest number of infected, we want 18.5 to be the max
     real_day_of_peak <- sum(all_peak_days) / length(all_peak_days)
     c(greatest_infected_number, real_day_of_peak)
 }
 
 ## Constants
-#the sizes of the 3 population samples we will consider
+# The sizes of the 3 population samples we will consider
 pop_size <- 100000
 cautious_pop_size <- pop_size / 10
 rand_pop_size <- pop_size / 1000
@@ -116,15 +114,16 @@ beta_prob <- beta / mean(beta)
 p_leaving_infected <- 1 / 5
 p_leaving_exposed <- 1 / 3
 
-#finds the lowest 10% of betas and the indicies of the corresponding people
+# Find the lowest 10% of betas and the indicies of the corresponding people
 ordered_beta <- sort(beta_prob)
-cautious_beta <- ordered_beta[c(1: pop_size/10)]
+cautious_beta <- ordered_beta[c(1: pop_size / 10)]
 cautious_index <- match(cautious_beta, beta_prob)
 
-#finds the cut off beta value, we only want people with betas lower than this value
-max_beta <- ordered_beta[pop_size/10]
+# Find the cut off beta value
+# because we only want people with betas lower than this value
+max_beta <- ordered_beta[pop_size / 10]
 
-#to store total infections on each day for each of the 10 simulations
+# Storage for total infections on each day for each of the 10 simulations
 ten_simulations <- matrix(data = 0, nrow = 10, ncol = simulation_days)
 
 day_of_peak <- matrix(data = 0, nrow = no_of_runs, ncol = 3)
@@ -132,57 +131,58 @@ value_of_peak <- matrix(data = 0, nrow = no_of_runs, ncol = 3)
  
 for (j in 1:no_of_runs) {
 
-    #a vector of randomly ordered integers from 1 to the size of the whole population with no repeats
+    # A vector of randomly ordered integers from 1 to the size of the whole population with no repeats
     random <- sample(c(1:pop_size), pop_size, replace = FALSE)
 
-    #Each row in population_states represent an individual in our model
-    #Column 1: the state the individual is in, we set this to be 0 for all individuals
-    #Column 2: the beta values we generated earlier
-    #Column 3: the [population size] randomly ordered integers
+    # Each row in population_states represent an individual in our model
+    # Column 1(state):  The state the individual is in, ividuals all start
+    #                   with 0 except 10 randomly chosen.
+    # Column 2(beta):    The beta values we generated earlier
+    # Column 3(rand):    [population size] randomly ordered integers
     # Valid states are the following:
     # 0:= Susceptible, 1:= Exposed, 2:= Infected, 3:= Removed(Recovered or dead)
     pop_states <- data.frame(state = rep(0, pop_size), beta = beta_prob, rand = random )
 
-    #Choose 10 random people to be exposed
+    # Choose 10 random people to be exposed
     starting_exposed <- sample(1:pop_size, 10)
     pop_states[starting_exposed, "state"] <- 1
 
-    #creates data frames for state_changes for the 3 population samples
+    # Create matrix for each of the 3 population samples.
+    # The matrix keeps track of how many people are infected,
+    # how many are 
     # Each row in daily_state_changes is the difference how many individuals
     # were in that state between the start of the day and the end of the day.
     daily_state_changes <- matrix(data = 0, nrow = simulation_days, ncol = 4)
     cautious_daily_changes <- matrix(data = 0, nrow = simulation_days, ncol = 4)
     random_samp_changes <- matrix(data = 0, nrow = simulation_days, ncol = 4)
 
-
     for (i in 1:simulation_days) {
-        
         new_states <- update_states(pop_states, lambda, p_leaving_infected, p_leaving_exposed)
 
-        #finds the cautious people in pop_states and new_states
-        cautious_pop <- pop_states[pop_states$beta <= max_beta,]
-        cautious_new <- new_states[pop_states$beta <= max_beta,]
-        
-        #to find a 0.1% random sample we choose people with index lower than this value 
+        # Find the cautious people in pop_states and new_states
+        cautious_pop <- pop_states[pop_states$beta <= max_beta, ]
+        cautious_new <- new_states[pop_states$beta <= max_beta, ]
+
+        # Take a 0.1% random sample by choosing people with indices
+        # lower than this value
         max_rand <- rand_pop_size
 
-        #finds people in the random 0.1% sample
-        rand_pop <- pop_states[pop_states$rand <= max_rand,]
-        rand_new <- new_states[new_states$rand <= max_rand,]
+        # Find people in the random 0.1% sample
+        rand_pop <- pop_states[pop_states$rand <= max_rand, ]
+        rand_new <- new_states[new_states$rand <= max_rand, ]
 
-        #update the corresponding data frame of state changes on each day for each of the 3 populations
+        # Update the corresponding data frame of state changes on each day for each of the 3 populations
         daily_state_changes <- calc_state_changes(pop_states, new_states, daily_state_changes, i, pop_size)
         cautious_daily_changes <- calc_state_changes(cautious_pop, cautious_new, cautious_daily_changes, i, cautious_pop_size)
         random_samp_changes <- calc_state_changes(rand_pop, rand_new, random_samp_changes, i, rand_pop_size)
         
-        #set the states of each individual today, as yesterday's states, ready for the next day of the simulation
+        # Set the states of each individual today, as yesterday's states, ready for the next day of the simulation
         pop_states <- new_states
         
         # if (i %% 10 == 0){
         #     ten_simulations[j, i/10] <- daily_state_changes[i, 4]
         # }
         ten_simulations[j, i] <- daily_state_changes[i, 4]
-        
     }
 
     total_pop_peak_data <- peak_day_and_total_infections(daily_state_changes)
@@ -192,7 +192,7 @@ for (j in 1:no_of_runs) {
     cautious_pop_peak_data <- peak_day_and_total_infections(cautious_daily_changes)
     value_of_peak[j, 2] <- cautious_pop_peak_data[1] / cautious_pop_size
     day_of_peak[j, 2] <- cautious_pop_peak_data[2]
-    
+
     random_pop_peak_data <- peak_day_and_total_infections(random_samp_changes)
     value_of_peak[j, 3] <- random_pop_peak_data[1] / rand_pop_size
     day_of_peak[j, 3] <- random_pop_peak_data[2]
